6f50ce128295ea9c718856cf21916d6a8f4600fc
Utsav Rai <utsavrai890@gmail.com>
2024-11-23 16:31:03 +0530

    initial setup for book management app

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Book Management App");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
